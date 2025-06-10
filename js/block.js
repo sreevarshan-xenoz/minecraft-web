4368de3b88e8e41ce52d10e01ff066d04c82b03f
sreevarshan-xenoz <sreevarshan1511@gmail.com>
2025-06-10 11:09:45 +0530

    feat: add initial Minecraft-like web game setup
    - Add HTML, CSS and JavaScript files for basic Three.js setup
    - Include texture assets for grass, dirt and stone blocks
    - Implement simple rotating cube animation as placeholder
    - Set up responsive canvas that adjusts to window resize

---
+++ js/block.js
@@ -0,0 +1,79 @@
+/**
+ * Block class for handling Minecraft-style blocks
+ */
+class Block {
+    constructor(type, position) {
+        this.type = type;
+        this.position = position;
+        this.mesh = null;
+        this.createMesh();
+    }
+
+    /**
+     * Create a Three.js mesh for this block based on its type
+     */
+    createMesh() {
+        // Create geometry (all blocks are 1x1x1 cubes)
+        const geometry = new THREE.BoxGeometry(1, 1, 1);
+        
+        // Create materials based on block type
+        let materials = [];
+        
+        switch(this.type) {
+            case 'grass':
+                // Create materials for each face of the cube
+                // Top face is grass, sides are dirt with grass on top, bottom is dirt
+                const topTexture = new THREE.TextureLoader().load('textures/grass.svg');
+                const sideTexture = new THREE.TextureLoader().load('textures/dirt.svg');
+                
+                // Create an array of materials for each face
+                materials = [
+                    new THREE.MeshBasicMaterial({ map: sideTexture }), // right face
+                    new THREE.MeshBasicMaterial({ map: sideTexture }), // left face
+                    new THREE.MeshBasicMaterial({ map: topTexture }),  // top face
+                    new THREE.MeshBasicMaterial({ map: sideTexture }), // bottom face
+                    new THREE.MeshBasicMaterial({ map: sideTexture }), // front face
+                    new THREE.MeshBasicMaterial({ map: sideTexture })  // back face
+                ];
+                break;
+                
+            case 'dirt':
+                const dirtTexture = new THREE.TextureLoader().load('textures/dirt.svg');
+                materials = Array(6).fill(new THREE.MeshBasicMaterial({ map: dirtTexture }));
+                break;
+                
+            case 'stone':
+                const stoneTexture = new THREE.TextureLoader().load('textures/stone.svg');
+                materials = Array(6).fill(new THREE.MeshBasicMaterial({ map: stoneTexture }));
+                break;
+                
+            default:
+                // Default is a simple colored cube
+                materials = Array(6).fill(new THREE.MeshBasicMaterial({ color: 0xAAAAAA }));
+        }
+        
+        // Create mesh with geometry and material
+        this.mesh = new THREE.Mesh(geometry, materials);
+        
+        // Set position
+        this.mesh.position.set(this.position.x, this.position.y, this.position.z);
+    }
+
+    /**
+     * Add this block to a Three.js scene
+     */
+    addToScene(scene) {
+        if (this.mesh) {
+            scene.add(this.mesh);
+        }
+    }
+
+    /**
+     * Remove this block from a Three.js scene
+     */
+    removeFromScene(scene) {
+        if (this.mesh) {
+            scene.remove(this.mesh);
+        }
+    }
+}
